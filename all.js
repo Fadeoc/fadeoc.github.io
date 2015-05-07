c12b3e52f030bc9c7a4b31a8a6b099ef7f8ab2b9
Fadeoc Khaos <fadeoc@gmail.com>
2015-05-08 00:20:53 +0800

    fix4
    a fix of the scroll to top button, replace the html anchor with a js
    function to make sure it will lead to the very top, not 30px below.

--- all.js
+++ all.js
@@ -1,5 +1,8 @@
+addLoadEvent(toTopSpan);
+
 window.onscroll = function () {
 	var navbarEle = document.getElementById("navbar");
+	var toTopEle = document.getElementById("toTop");
 	var navbarOffset = navbarEle.offsetTop;
 	var parentOffset = navbarEle.offsetParent;
 	if (parentOffset !== null) {
@@ -12,4 +15,31 @@ window.onscroll = function () {
 	} else {
 		navbarEle.className = "";
 	}
+	if(navbarScroll > 1000){
+		toTopEle.style.visibility = "visible";
+		toTopEle.style.pointerEvents = "auto";
+	}else{
+		toTopEle.style.visibility = "hidden";
+		toTopEle.style.pointerEvents = "none";
+	}
 };
+
+function toTopSpan(){
+	var toTopF = document.getElementById("toTop");
+	toTopF.onclick = function(){
+		window.scrollTo(0,0);
+	};
+}
+
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != "function") {
+		window.onload = func;
+	}
+	else {
+		window.onload = function () {
+			oldonload();
+			func();
+		};
+	}
+}
