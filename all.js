59134c8967cc21dc24dc5ee3b7b96e92b13f98a5
Fadeoc Khaos <fadeoc@gmail.com>
2015-05-17 12:35:06 +0800

    img slider complete
    hell yeah!

--- all.js
+++ all.js
@@ -1,4 +1,5 @@
 addLoadEvent(toTopSpan);
+addLoadEvent(imgSlider);
 
 window.onscroll = function () {
 	var navbarEle = document.getElementById("navbar");
@@ -15,19 +16,44 @@ window.onscroll = function () {
 	} else {
 		navbarEle.className = "";
 	}
-	if(navbarScroll > 1000){
+	if (navbarScroll > 1000) {
 		toTopEle.style.visibility = "visible";
 		toTopEle.style.pointerEvents = "auto";
-	}else{
+	} else {
 		toTopEle.style.visibility = "hidden";
 		toTopEle.style.pointerEvents = "none";
 	}
 };
 
-function toTopSpan(){
+function toTopSpan() {
 	var toTopF = document.getElementById("toTop");
-	toTopF.onclick = function(){
-		window.scrollTo(0,0);
+	toTopF.onclick = function () {
+		window.scrollTo(0, 0);
+	};
+}
+
+function imgSlider() {
+	var slides = document.getElementsByClassName("slides");
+	var maskWidth = 648;
+	Array.prototype.forEach.call(slides, function (el, i) {
+		el.style.left = (i * maskWidth) + "px";
+	});
+
+	window.preNext = function (x) {
+		var widthCounter = maskWidth;
+		if ((x == 'imgnext' && (parseInt(slides[0].style.left) + maskWidth * (slides.length - 1) > 0)) || (x == 'imgpre' && (parseInt(slides[0].style.left) < 0))) {
+			var slideWithdraw = setInterval(function () {
+				var moveSpeed = 4;
+				Array.prototype.forEach.call(slides, function (el, i) {
+					if (widthCounter <= 0) {
+						clearInterval(slideWithdraw);
+					} else {
+						el.style.left = (x == 'imgnext') ? (parseInt(el.style.left) - moveSpeed) + "px" : (parseInt(el.style.left) + moveSpeed) + "px";
+					}
+				});
+				widthCounter -= moveSpeed;
+			}, 1);
+		}
 	};
 }
 
